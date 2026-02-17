757690fd78dcf96ab409ceaa74cf5fa5429a9191
akshata_yatnal <yatnalakshata@gmail.com>
2026-02-17 14:56:26 +0530

    Add files via upload

---
+++ simply.data.py
@@ -0,0 +1,9 @@
+name="Akhata"
+age=21
+study="as a B.tech ECE"
+background="agriculture background family"
+
+print("my name is",name,"I am",age,"year old.",
+      "I am currenty persuing ",study,"student.",
+      "I came from",background)
+print("thankyou for the opportunity")
