abd2faa72b9d8dcadda362eb0f841e62ba759598
akshata_yatnal <yatnalakshata@gmail.com>
2026-02-21 16:06:32 +0530

    Add files via upload

---
+++ break.1.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    if i==8:
+        print('process is enough,break loop execution...')
+        break
+    print(i)
+print('outside of loop')
---
+++ brk2.py
@@ -0,0 +1,6 @@
+cart=[10,20,30,600,700,800,70,90]
+for item in cart:
+    if item>700:
+        print("to place order insurence must required,we cant provide")
+        break
+    print("processing item:",item)
