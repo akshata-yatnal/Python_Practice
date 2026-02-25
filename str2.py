3f3bb9d784bf3efc82ce0a4a663189dba63a3be5
akshata_yatnal <yatnalakshata@gmail.com>
2026-02-25 17:25:19 +0530

    Add files via upload

---
+++ str2.py
@@ -0,0 +1,4 @@
+a=input("enter a some string to reverse:")
+r=reversed(a)
+output=' '.join(r)
+print(output)
---
+++ str4.py
@@ -0,0 +1,5 @@
+s=input("enter a some string :")
+l=s.split()
+l1=l[ : :-1]
+output=' '.join(l1)
+print(output)
