d22c5e3bccb13bb92fed1d25b44f03213a9fa300
akshata_yatnal <yatnalakshata@gmail.com>
2026-05-03 23:42:10 +0530

    Add files via upload

---
+++ forloop_string.py
@@ -0,0 +1,5 @@
+# for loop USING CDT:
+# for loop string CDT:
+s='BYE'
+for ele in s:
+    print(ele)
---
+++ forloop_tuple.py
@@ -0,0 +1,6 @@
+# for loop USING CDT:
+# for loop TUPLE CDT:
+t= 11,22,'hi',[22,33]
+for ele in t:
+    print(ele)
+ 
