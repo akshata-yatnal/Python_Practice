ac2ce455aac9c45bc6f5dc358e0f6941b9ffd131
akshata_yatnal <yatnalakshata@gmail.com>
2026-05-08 16:57:26 +0530

    Add files via upload

---
+++ if.else.largst num.py
@@ -0,0 +1,9 @@
+n1=int(input('enter 1st number:'))
+n2=int(input('enter a 2nd number:'))
+if n1>n2:
+       print(n1)
+       print('{} is largest number'.format(n1))
+else:
+    print(n2)
+    print('{} is largest number'.format(n2))
+    
---
+++ if1.cond.py
@@ -0,0 +1,4 @@
+n=int(input("enter a given number:"))
+if n>100:
+    print(n)
+    
