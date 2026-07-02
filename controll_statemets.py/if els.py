00d928098eada08cf2575fdba94cdeb7394c6633
akshata_yatnal <yatnalakshata@gmail.com>
2026-07-02 13:42:56 +0530

    Add files via upload

---
+++ controll_statemets.py/if els.py
@@ -0,0 +1,7 @@
+name=input('enter a name:')
+if name=='akshata':
+    print('hello akshata,good evening')
+else:
+    print('hello guest,good evening')
+print('how r u!!!')
+    
---
+++ controll_statemets.py/nted1.py
@@ -0,0 +1,4 @@
+for i in range(3):#0,1,2
+    for j in range (2):#0,1
+        print("hello")
+        
